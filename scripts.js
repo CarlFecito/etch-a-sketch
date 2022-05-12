7d43d3192f1c04f7f8bb3949fcc2615a2aa4b846
CarlFecito <cmolinaf96@gmail.com>
2022-05-11 23:54:36 -0400

    now can change grid size

--- scripts.js
+++ scripts.js
@@ -14,8 +14,22 @@ function addDiv(id) {
     cont.appendChild(newDiv)
 }
 
+function pixelSize(amount) {
+    
+}
+
 
 // WORKING ON THIS
 function changeColor() {
     document.addEventListener("mouseover", )
+}
+
+function clickGrid() {
+    let gridSize = parseInt(prompt(`Change grid size 1 ~ 100`, `4`))
+    if (gridSize > 0 && gridSize <= 100) {
+        document.getElementById('contain').innerHTML = ''
+        drawGrid(gridSize)
+    } else {
+        alert('Grid size must be a number between 1 and 100')
+    }
 }
